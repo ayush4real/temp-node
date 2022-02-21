2b5703c5c92e9773552a36b69320fec04a47ad86
Ayush <ayushdps99@gmail.com>
2022-02-21 16:32:07 +0530

    Updated files for methods and middleware options

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,33 +1,84 @@
 // Middlewares are functions that execute during requests to the browser.
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const logger = require('./logger.js');
-const authorize = require('./authorize.js');
+let { people } = require("./data");
 
-// below line will apply logger to all the paths starting with /api (only).
-// app.use('/api', logger);
+// static assets
+app.use(express.static("./methods-public"));
 
-app.use([logger, authorize]);
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+// parse json
+app.use(express.json());
 
-app.get('/', (req,res)=>{
+app.post("/login", (req, res) => {
+  // console.log(req.body);
+  const { name } = req.body;
+
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+
+  res.status(401).send("please enter valid credentials");
+});
+
+app.get("/api/people", (req, res) => {
+    res.status(200).json({ success: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+    const {name} = req.body;
     
-    res.send('home');
+    if(!name){
+        return res.status(400).json({success: false, msg:'please provide name value'});
+    }
+  res.status(201).json({success: true, person: name});
+});
+
+app.post('/api/people/postman', (req,res)=>{
+    const {name} = req.body;
+
+    if(!name){
+        res.status(400).json({success: false, msg: 'please provide name value'});
+    }
+
+    res.status(200).json({success: true, data: [...people, name]})
 })
 
-app.get('/about', (req,res)=>{
-    res.send('about');
+
+app.put('/api/people/:id', (req,res) => {
+    const {id} = req.params;
+    const {name} = req.body;
+
+    const person = people.find(person => person.id === Number(id));
+
+    if(!person){
+        return res.status(404).json({success: false, msg: `No person with id ${id}`});
+    }
+    const newPeople = people.map((person) => {
+        if(person.id === Number(id)){
+            person.name = name;
+        }
+        return person
+    });
+
+    res.status(200).json({success: true, data: newPeople});
+
+    // console.log(id, name);
+    // res.send('hello world');
 })
 
-app.get('/api/products', (req,res)=>{
-    res.send('products');
+app.delete('/api/people/:id', (req,res) => {
+    const person = people.find(person => person.id === Number(req.params.id));
+    if(!person){
+        res.status(404).json({success: false, msg: `No person with id ${req.params.id}`});
+    }
+    const newPeople = people.filter((person)=>person.id !== Number(req.params.id));
+    return res.status(200).json({success: true, data:newPeople});
 })
 
-app.get('/api/items', (req,res)=>{
-    console.log(req.user);
-    res.send('items');
-})
+app.listen(5000, () => {
+  console.log("listening on port 5000...");
+});
 
-app.listen(5000, ()=>{
-    console.log('listening on port 5000...');
-})
--- 02-express-tutorial/final/10-middleware-options.js
+++ 02-express-tutorial/final/10-middleware-options.js
@@ -26,3 +26,45 @@ app.get('/api/items', (req, res) => {
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....')
 })
+
+// ------------------MY CODE----------------------
+// Middlewares are functions that execute during requests to the browser.
+
+// const express = require('express');
+// const app = express();
+// const morgan = require('morgan');
+// const logger = require('./logger.js');
+// const authorize = require('./authorize.js');
+
+// // req => middleware => res
+
+// // 1. use of route
+// // 2. options - our own / express / third party
+
+// // below line will apply logger to all the paths starting with /api (only).
+// // app.use('/api', logger);
+
+// // app.use([logger, authorize]);
+// app.use(morgan('tiny'));
+
+// app.get('/', (req,res)=>{
+    
+//     res.send('home');
+// })
+
+// app.get('/about', (req,res)=>{
+//     res.send('about');
+// })
+
+// app.get('/api/products', (req,res)=>{
+//     res.send('products');
+// })
+
+// app.get('/api/items',(req,res)=>{
+//     console.log(req.user);
+//     res.send('items');
+// })
+
+// app.listen(5000, ()=>{
+//     console.log('listening on port 5000...');
+// })
